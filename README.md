6c1bc054751e046839c8f0bf637ed487efbadacf
Ayanamii14 <Ayanamii14@users.noreply.github.com>
2017-09-22 13:53:26 +0800

    Add files via upload
    php文件修改

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # socketTongXun
 
+不用任何框架，使用基于```c```的```socket```来实现
--- socketServer3.php
+++ socketServer3.php
@@ -4,10 +4,8 @@ set_time_limit(0);
 
 $address = "127.0.0.1";
 $port = 7777;
-$i = 0;
-$arr = array();
 
-$mysqli = new mysqli("localhost","root","root","StudentsDB");
+$mysqli = new mysqli("localhost","root","root","studentDB");
 
 //创建端口
 $sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("socket_create() failed :reason:" . socket_strerror(socket_last_error()) . "\n");
@@ -20,27 +18,30 @@ socket_listen($sock, 5) or die("socket_bind() failed :reason:" . socket_strerror
 
 do {
 	//得到一个链接
-	if (($msgsock = socket_accept($sock)) === false) {
-		echo "socket_accepty() failed :reason:" . socket_strerror(socket_last_error($sock)) . "\n";
-		break;
-	}
+    if (($msgsock = socket_accept($sock)) === false) {
+        echo "socket_accepty() failed :reason:" . socket_strerror(socket_last_error($sock)) . "\n";
+        break;
+    }
 	//welcome  发送到客户端
-	$msg = "hello";
-	socket_write($msgsock, $msg, strlen($msg));
+    $msg = "hello";
+    socket_write($msgsock, $msg, strlen($msg));
 	// read client message
 	do {
+        $i = 0;
+        $arr = array();
 		$buf = socket_read($msgsock, 8192);
+        //buf为空跳出
 		if (!$buf) {
 			break;
 		}
-
+        //select
 		if (strstr($buf,"SELECT")) {
 			$result = $mysqli -> query($buf);
 			while ($row = $result -> fetch_assoc()) {
-				$arr[$i]["st_name"] = $row["st_name"];
-				$arr[$i]["st_gender"] = $row["st_gender"];
-				$arr[$i]["st_age"] = $row["st_age"];
-				$arr[$i]["st_studentID"] = $row["st_studentID"];
+				$arr[$i]["name"] = $row["name"];
+				$arr[$i]["gender"] = $row["gender"];
+				$arr[$i]["age"] = $row["age"];
+				$arr[$i]["studentID"] = $row["studentID"];
 				$i ++;
 			}
 
@@ -49,12 +50,18 @@ do {
 				"data" => $arr,
 			));
 		}
+        //insert
+        if (strstr($buf,"INSERT INTO")) {
+            
+        }
+        
 		if ($json) {
 			$talkback = $json;
 		}
 		else {
 			$talkback = "none";
 		}
+        
 		if (false === socket_write($msgsock, $talkback, strlen($talkback))) {
 			echo "socket_write() failed reason:" . socket_strerror(socket_last_error($sock)) ."\n";
 		} else {
