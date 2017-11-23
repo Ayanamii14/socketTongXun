6f480b69b2c089060886701c256437086c282106
lyhao <lyhao@xinlonghang.cn>
2017-11-23 15:14:57 +0800

    1. O__O "…

---
+++ cfstreamMsg/vieww.h
@@ -0,0 +1,13 @@
+//
+//  vieww.h
+//  cfstreamMsg
+//
+//  Created by lyhao on 2017/11/16.
+//  Copyright © 2017年 lyhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface vieww : UIView
+
+@end
---
+++ cfstreamMsg/vieww.m
@@ -0,0 +1,23 @@
+//
+//  vieww.m
+//  cfstreamMsg
+//
+//  Created by lyhao on 2017/11/16.
+//  Copyright © 2017年 lyhao. All rights reserved.
+//
+
+#import "vieww.h"
+
+@implementation vieww
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+
+
+@end
